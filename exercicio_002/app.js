896dcfa73d4e9e3c96c1d84a16f2d0e487a5d9a5
Anderson Silva <andersonsoufeliz27@gmail.com>
2024-02-15 17:17:16 -0300

    exercicio02_resolvido

--- exercicio_002/app.js
+++ exercicio_002/app.js
@@ -8,3 +8,30 @@ Enunciado: CONTADOR COM INTERVALO DE VALORES
     O valor nunca poderá ser inferior a -10 ou superior a 10.
 
 ---------------------------------------------------------------------------- */
+const btn_decremento = document.querySelector('#btn_decremento')
+const btn_incremento = document.querySelector('#btn_incremento')
+const texto = document.querySelector('.text-warning')
+
+let valor_decremento =1;
+let valor_incremento =1;
+if(btn_decremento){
+btn_decremento.addEventListener('click',(evt)=>{
+console.log(evt.target);
+if(valor_decremento > -10){
+let conversao = parseInt(texto.innerHTML);
+valor_decremento =  --conversao;
+texto.innerHTML = valor_decremento;
+}
+ })
+}
+
+if(btn_incremento){
+    btn_incremento.addEventListener('click',(evt)=>{
+    console.log(evt.target);
+    if(valor_incremento < 10){
+    let conversao = parseInt(texto.innerHTML);
+    valor_incremento =  ++conversao;
+    texto.innerHTML = valor_incremento;
+    }
+     })
+    }

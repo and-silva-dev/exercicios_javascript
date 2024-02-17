adcc04e1e9ee60aa68779e05b3023756fb89893a
Anderson Silva <andersonsoufeliz27@gmail.com>
2024-02-17 17:33:57 -0300

    exercício 6 resolvido

--- exercicio_006/app.js
+++ exercicio_006/app.js
@@ -9,3 +9,32 @@ Enunciado:
 
     NOTA: quando uma área é apresentada, as outras devem ficar escondidas
 ---------------------------------------------------------------------------- */
+const tab1 = document.querySelector("#tab1");
+const tab2 = document.querySelector("#tab2");
+const tab3 = document.querySelector("#tab3");
+const info1 = document.querySelector("#info1");
+const info2 = document.querySelector("#info2");
+const info3 = document.querySelector("#info3");
+
+tab1.addEventListener("click", (event) => {
+  reset();
+  info2.classList.add("dpnone");
+  info3.classList.add("dpnone");
+});
+
+tab2.addEventListener("click", (event) => {
+  reset();
+  info1.classList.add("dpnone");
+  info3.classList.add("dpnone");
+});
+tab3.addEventListener("click", (event) => {
+  reset();
+  info1.classList.add("dpnone");
+  info2.classList.add("dpnone");
+});
+
+function reset() {
+  info1.classList.remove("dpnone");
+  info2.classList.remove("dpnone");
+  info3.classList.remove("dpnone");
+}

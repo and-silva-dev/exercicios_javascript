22aa802249e5270496c25cff8a34f01cabe1337e
Anderson Silva <andersonsoufeliz27@gmail.com>
2024-03-01 21:56:27 -0300

    exercício 12

--- exercicio_012/app.js
+++ exercicio_012/app.js
@@ -5,3 +5,7 @@ Enunciado:
     Ao clicar no botão "Adicionar", se o input de texto não estiver vazio, adicionar
     um parágrafo com esse texto por baixo do botão.
 ---------------------------------------------------------------------------- */
+
+const text_post = document.querySelector('#text_post')
+const btn = document.querySelector('btn')
+const post = document.querySelector('.posts')
